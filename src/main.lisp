1077460f857932321f59d337d1da3a92c12b9ee3
Benjamin Saunders <besaunde@sfu.ca>
2010-02-26 01:04:39 -0800

    Working intial test.

--- src/main.lisp
+++ src/main.lisp
@@ -1,3 +1,17 @@
 (in-package :thune2)
 
-
+(defun main (&aux
+             (conf (load-conf "./thune2.conf"))
+             (connection (ircl:connect (conf-value conf :server))))
+  (ircl:send-message connection (ircl:make-message "NICK" (conf-value conf :nick)))
+  (ircl:send-message connection (ircl:make-message "USER"
+                                                   (conf-value conf :user)
+                                                   "." "."
+                                                   (conf-value conf :real-name)))
+  (ircl:send-message connection (ircl:make-message "JOIN"
+                                                   (reduce (lambda (accum channel)
+                                                             (concatenate 'string
+                                                                          accum "," channel))
+                                                           (conf-value conf :channels))))
+  (sleep 10)
+  (ircl:send-message connection (ircl:make-message "QUIT")))
