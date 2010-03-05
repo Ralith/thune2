63c9663d878d21ad52e384012a1db4ceda928c52
Benjamin Saunders <besaunde@sfu.ca>
2010-03-04 21:01:34 -0800

    Added time interval formatting and string case macro to utils.

--- src/utils.lisp
+++ src/utils.lisp
@@ -50,3 +50,61 @@
     (when (> length 0)
       (char= (code-char 1)
              (aref string 0) (aref string (1- length))))))
+
+;;; TODO: Take a list of var-factor pairs
+(defmacro %format-interval-distribute (vars factors)
+  "Distributes (FIRST VARS) amongst (REST VARS) such that the value of each VAR is less than its corresponding FACTOR.  The factor corresponding to the last element of VARS, if any, is ignored to ensure support for large values."
+  (let ((stack (gensym))
+        (result (gensym))
+        (remainder (gensym)))
+   `(let ((,stack (quote ,factors)))
+      ,@(mapcar (lambda (lower higher)
+                  `(multiple-value-bind (,result ,remainder)
+                       (floor ,lower (pop ,stack))
+                     (setf ,higher ,result)
+                     (setf ,lower ,remainder)))
+                vars (rest vars)))))
+
+(defmacro %format-interval-string (vars names)
+  "Returns a human-readable string of the values of VARS referred to with their respective NAMES."
+  (let ((in-string (gensym))
+        (string (gensym)))
+    `(let ((,in-string nil)
+           (,string))
+       ,@(mapcar (lambda (var name)
+                  (let ((control-string (format nil "~~a ~a~~:p" name)))
+                    `(when (> ,var 0)
+                       (setf ,string
+                             (concatenate 'string
+                                          ,string
+                                          (when ,in-string ", ")
+                                          (format nil ,control-string ,var)))
+                       (setf ,in-string t))))
+                 vars names)
+       ,string)))
+
+(defun format-interval (seconds)
+  "Returns a human-readable string representing the duration SECONDS in years, months, weeks, days, hours, minutes, and seconds, omitting zero values."
+  (if (< seconds 1)
+      "no time"
+      (let ((years) (months) (weeks) (days) (hours) (minutes))
+        (%format-interval-distribute (seconds minutes hours days weeks months years)
+                                     (60 60 24 7 4 12))
+        (%format-interval-string (years months weeks days hours minutes seconds)
+                                 ("year" "month" "week" "day" "hour" "minute" "second")))))
+
+
+(defmacro strcase (string &body cases)
+  `(cond
+     ,@(mapcar (lambda (case &aux
+                         (key (first case))
+                         (code (second case)))
+                 
+                 `(,(cond
+                     ((listp key)
+                      `(or ,@(mapcar (curry #'list 'string= string)
+                                     key)))
+                     ((eq key t) t)
+                     (t `(string= ,string ,key)))
+                    ,code))
+               cases)))
